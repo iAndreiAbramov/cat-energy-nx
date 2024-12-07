104dfb0d399bdb533cf88e71ea06f3f71496a947
Abramov <anabramov@ptsecurity.com>
2024-12-07 22:45:10 +0300

    Initial commit

---
+++ apps/cat-energy-nx/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'cat-energy-nx',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/cat-energy-nx',
+};
